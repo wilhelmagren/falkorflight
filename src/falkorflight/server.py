07d986ca762b2e4618db5acf4ea57a3acf074367
wilhelmagren <wilhelmagren98@gmail.com>
2025-05-02 13:22:54 +0200

    feat: falkor flight server methods

--- src/falkorflight/server.py
+++ src/falkorflight/server.py
@@ -1,8 +1,10 @@
 import json
 from typing import (
+    Iterable,
     List,
     Optional,
     Tuple,
+    Union,
 )
 
 from falkordb import FalkorDB
@@ -17,10 +19,12 @@ class FalkorFlightServer(flight.FlightServerBase):
         falkordb_host: str,
         falkordb_port: str,
         *,
+        falkordb_username: Optional[str] = None,
+        falkordb_password: Optional[str] = None,
         flight_host: str = "0.0.0.0",
         flight_port: str = "8815",
-        tls: bool = True,
-        verify_client: bool = True,
+        tls: bool = False,
+        verify_client: bool = False,
         tls_certificates: Optional[List[Tuple[str, str]]] = None,
         root_certificates: Optional[bytes] = None,
     ) -> None:
@@ -29,7 +33,7 @@ class FalkorFlightServer(flight.FlightServerBase):
         if tls:
             scheme = "grpc+tls"
 
-        flight_url = f"{scheme}://{flight_host}:{flight_port}",
+        flight_url = f"{scheme}://{flight_host}:{flight_port}"
 
         super(FalkorFlightServer, self).__init__(
             location=flight_url,
@@ -41,6 +45,8 @@ class FalkorFlightServer(flight.FlightServerBase):
         self._falkordb_host = falkordb_host
         self._falkordb_port = falkordb_port
         self._falkordb_url = f"http://{falkordb_host}:{falkordb_port}"
+        self._falkordb_username = falkordb_username
+        self._falkordb_password = falkordb_password
 
         self._flight_host = flight_host
         self._flight_port = flight_port
@@ -53,23 +59,59 @@ class FalkorFlightServer(flight.FlightServerBase):
         # Apache Arrow Flight does not yet have support for asyncio, but
         # there is a tracking issue for this on the arrow GitHub repo:
         # https://github.com/apache/arrow/issues/34607
-        #
-        # Also, FalkorDB has support for authentication using password or ssl_certs
-        # maybe should think about using those if possible?
-        self._falkordb_client = FalkorDB(host=falkordb_host, port=falkordb_port)
+        self._falkordb_client = FalkorDB(
+            host=falkordb_host,
+            port=falkordb_port,
+            username=falkordb_username,
+            password=falkordb_password,
+        )
+
+    def do_action(
+        self,
+        context: flight.ServerCallContext,
+        action: flight.Action,
+    ) -> Iterable[bytes]:
+        """Execute a custom command."""
+        if action.type == "do_delete":
+            self.do_delete_graph(action.body.to_pybytes().decode("utf-8"))
+        else:
+            raise ValueError(f"Unknown action: {action.type}")
 
     def do_get(
         self,
         context: flight.ServerCallContext,
         ticket: flight.Ticket,
     ) -> flight.FlightDataStream:
-        """Perform an OpenCypher on a FalkorDB graph."""
+        """Perform an OpenCypher query on a FalkorDB graph."""
 
         payload = json.loads(ticket.ticket.decode("utf-8"))
         graph_name = payload["graph"]
         cypher_query = payload["query"]
 
-        g = self._falkordb_client.select_graph(graph_name)
-        result = g.query(cypher_query)
+        graph = self._falkordb_client.select_graph(graph_name)
+        result = graph.query(cypher_query)
 
-        # TODO: process result and return to user
+        print(result.header)
+
+    def do_put(
+        context: flight.ServerCallContext,
+        descriptor: flight.FlightDescriptor,
+        reader: flight.MetadataRecordBatchReader,
+        writer: flight.FlightMetadataWriter,
+    ) -> None:
+        """Write data to a flight."""
+        raise NotImplementedError
+
+    def list_actions(
+        self,
+        context: flight.ServerCallContext,
+    ) -> Union[Iterable[flight.ActionType], Tuple[str, str]]:
+        """List custom actions available on this server."""
+        return ("delete_graph", "Delete a graph from FalkorDB.")
+
+
+    def do_delete_graph(self, graph_name: str) -> None:
+        """Delete a graph from FalkorDB."""
+        graph = self._falkordb_client.select_graph(graph_name)
+        graph.query("MATCH (n) DETACH DELETE n")
+
